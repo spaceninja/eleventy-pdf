d0e3e5a4d212208ad8f30e8a2cdc599ee5996076
Scott Vandehey <fake-scott@spaceninja.com>
2023-08-17 12:13:50 -0700

    better CSS formatting and comments

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ['stylelint-config-spaceninja', 'stylelint-config-prettier'],
+  // 1. This rule doesn't understand some print styles.
+  rules: {
+    'scss/at-rule-no-unknown': null, // 1
+    'selector-pseudo-element-no-unknown': null, // 1
+  },
 };
